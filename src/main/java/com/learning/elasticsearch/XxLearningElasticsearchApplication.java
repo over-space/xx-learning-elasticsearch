ab606649d102a4d0a08f38dc4d0214047c371833
lifang <lifang@jingkunsystem.com>
2021-05-31 15:17:58 +0800

    init project

---
+++ src/main/java/com/learning/elasticsearch/XxLearningElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package com.learning.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxLearningElasticsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XxLearningElasticsearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/learning/elasticsearch/XxLearningElasticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.elasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XxLearningElasticsearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
